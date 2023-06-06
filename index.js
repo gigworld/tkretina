ebadbdddd1befaef00c49ae6de83e16a468d60b2
gigworld <gigworld2023@gmail.com>
2023-06-06 18:08:00 +0300

    added js file into index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 </select>
 </form>
 
+<<<<<<< HEAD
 
 <p>The id of the form is:<p>
 <p id="demo"></p>
@@ -22,5 +23,19 @@ document.getElementById("demo").innerHTML = document.getElementById("mySelect").
 </body>
 </html>
 
+=======
+<<<<<<< HEAD
+>>>>>>> 8ac2dbb (added js file into index.js)
 <p>This file has been modified:<p>
 <p id="demo"></p>
+=======
+<p>The id of the form is:<p>
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = document.getElementById("mySelect").form.id;
+</script>
+
+</body>
+</html>
+>>>>>>> 5a2d7db (added js file into index.js)
