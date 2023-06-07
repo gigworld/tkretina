d7460a2e89e593b97b0b42b9f822ad13e93352a0
gigworld <135525758+gigworld@users.noreply.github.com>
2023-06-07 15:38:28 +0300

    index.js has been updated but without the js block

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello Gig world")
+<!DOCTYPE html>
+<html>
+<body>
+
+<form id="myForm">
+<select id="mySelect">
+  <option>Apple</option>
+  <option>Pear</option>
+  <option>Banana</option>
+  <option>Orange</option>
+</select>
+</form>
+
+<p>This file has been modified:<p>
+<p id="demo"></p>
